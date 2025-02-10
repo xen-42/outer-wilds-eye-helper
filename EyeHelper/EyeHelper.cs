5522645f881d51eac9a5c00e47029ae4ca05e69d
xen-42 <nicholasconnors@hotmail.ca>
2025-02-09 21:46:52 -0500

    Add warping to ancient glade

--- EyeHelper/EyeHelper.cs
+++ EyeHelper/EyeHelper.cs
@@ -1,33 +1,73 @@
-﻿using OWML.Common;
+﻿using HarmonyLib;
+using OWML.Common;
 using OWML.ModHelper;
+using System.Collections;
+using UnityEngine;
 
 namespace EyeHelper
 {
+    [HarmonyPatch]
     public class EyeHelper : ModBehaviour
     {
+        public static EyeState eyeState;
+
         public override void SetupPauseMenu(IPauseMenuManager pauseMenu)
         {
             base.SetupPauseMenu(pauseMenu);
 
             if (LoadManager.GetCurrentScene() == OWScene.EyeOfTheUniverse)
             {
-                var solarSystemButton = pauseMenu.MakeSimpleButton("RETURN TO OUTER WILDS", 3, true);
-                solarSystemButton.OnSubmitAction += () =>
+                pauseMenu.MakeSimpleButton("RETURN TO OUTER WILDS", 3, true).OnSubmitAction += () =>
                 {
+                    // This just unpauses
                     Locator.GetSceneMenuManager().pauseMenu.OnSkipToNextTimeLoop();
                     PlayerData._currentGameSave.warpedToTheEye = false;
+                    eyeState = EyeState.Undefined;
                     LoadManager.LoadScene(OWScene.SolarSystem, LoadManager.FadeType.ToBlack, 0.5f);
                 };
             }
             if (LoadManager.GetCurrentScene() == OWScene.SolarSystem)
             {
-                var solarSystemButton = pauseMenu.MakeSimpleButton("GO TO THE EYE", 3, true);
-                solarSystemButton.OnSubmitAction += () =>
+                pauseMenu.MakeSimpleButton("GO TO THE EYE", 3, true).OnSubmitAction += () =>
                 {
+                    // This just unpauses
                     Locator.GetSceneMenuManager().pauseMenu.OnSkipToNextTimeLoop();
-                    PlayerData._currentGameSave.warpedToTheEye = true;
-                    LoadManager.LoadScene(OWScene.EyeOfTheUniverse, LoadManager.FadeType.ToBlack, 0.5f);
+                    eyeState = EyeState.AboardVessel;
+                    StartCoroutine(LoadEye());
                 };
+
+                pauseMenu.MakeSimpleButton("GO TO THE ANCIENT GLADE", 3, true).OnSubmitAction += () =>
+                {
+                    // This just unpauses
+                    Locator.GetSceneMenuManager().pauseMenu.OnSkipToNextTimeLoop();
+                    eyeState = EyeState.ForestIsDark;
+                    StartCoroutine(LoadEye());
+                };
+            }
+        }
+
+        private IEnumerator LoadEye()
+        {
+            // Wait a bit in case we're impatient and just woke up else it glitches out at the eye scene and disables movement
+            var cameraEffects = GameObject.FindObjectOfType<PlayerCameraEffectController>();
+            if (cameraEffects._isOpeningEyes)
+            {
+                yield return new WaitForSeconds(cameraEffects._eyeAnimDuration);
+            }
+
+            // Else it overrides the state
+            PlayerData._currentGameSave.warpedToTheEye = eyeState == EyeState.AboardVessel;
+            LoadManager.LoadScene(OWScene.EyeOfTheUniverse, LoadManager.FadeType.ToBlack, 0.5f);
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(EyeStateManager), nameof(EyeStateManager.Awake))]
+        public static void Patch(EyeStateManager __instance)
+        {
+            if (eyeState != EyeState.Undefined)
+            {
+                __instance._initialState = eyeState;
+                eyeState = EyeState.Undefined;
             }
         }
     }
